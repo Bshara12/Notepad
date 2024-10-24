922ecbefee7693e84d6e54c892ce733197bb1aad
Bshara12 <bshara.h2003@gmail.com>
2024-10-24 23:01:21 +0300

    create note Modle

--- lib/views/widgets/EditviewBody.dart
+++ lib/views/widgets/EditviewBody.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:notepad/views/widgets/custom_text_field.dart';
 
 class EditviewBody extends StatelessWidget {
   const EditviewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Container(
+        child:const Column(children: [
+          SizedBox(height: 50,),
+          customTextField(hint: 'Title'),
+          SizedBox(height: 50,),
+          customTextField(hint: 'content',maxlines: 5),
+        ],),
+      ),
+    );
   }
 }
